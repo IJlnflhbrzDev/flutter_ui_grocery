eefc1ab2901856864bf97e8da6c6b25f5131a1e9
ijlnflhbrzdev <ijlnflhbrzdev@gmail.com>
2023-02-15 23:46:38 +0700

    MENAMBAHKAN RESPONSIVE MODE WEB,DEKSTOP & MOBILE, PADAHALAHAN UTAMA PRODUCT

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -14,18 +14,68 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
-      child: GridView.builder(
-        itemCount: alldata.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          // JIKA KENA OVERFLOW SECARA DEFAULT childAspectRatio 1/1 atur kembali supaya tidak kena overlow
-          childAspectRatio: 0.7,
-          // crossAxisSpacing adalah Jarak antara element
-          crossAxisSpacing: 10,
-          // crossAxisCount adalah BERAPA ELEMENT YG INGIN DI GROUP
-          crossAxisCount: 2,
-        ),
-        itemBuilder: (context, index) {
-          return ItemWidget(products: alldata[index]);
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          bool isTablet = MediaQuery.of(context).size.width < 1100 &&
+              MediaQuery.of(context).size.width >= 850;
+          bool isDesktop = MediaQuery.of(context).size.width >= 1100;
+          bool isMobile = MediaQuery.of(context).size.width < 850;
+
+          if (isDesktop) {
+            return Container(
+              child: GridView.builder(
+                itemCount: alldata.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  // JIKA KENA OVERFLOW SECARA DEFAULT childAspectRatio 1/1 atur kembali supaya tidak kena overlow
+                  childAspectRatio: .7,
+                  // crossAxisSpacing adalah Jarak antara element
+                  crossAxisSpacing: 10,
+                  // crossAxisCount adalah BERAPA ELEMENT YG INGIN DI GROUP
+                  crossAxisCount: 6,
+                ),
+                itemBuilder: (context, index) {
+                  return ItemWidget(products: alldata[index]);
+                },
+              ),
+            );
+          }
+
+          if (isTablet) {
+            return Container(
+              child: GridView.builder(
+                itemCount: alldata.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  // JIKA KENA OVERFLOW SECARA DEFAULT childAspectRatio 1/1 atur kembali supaya tidak kena overlow
+                  childAspectRatio: 0.7,
+                  // crossAxisSpacing adalah Jarak antara element
+                  crossAxisSpacing: 10,
+                  // crossAxisCount adalah BERAPA ELEMENT YG INGIN DI GROUP
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return ItemWidget(products: alldata[index]);
+                },
+              ),
+            );
+          }
+
+          //mobile
+          return Container(
+            child: GridView.builder(
+              itemCount: alldata.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                // JIKA KENA OVERFLOW SECARA DEFAULT childAspectRatio 1/1 atur kembali supaya tidak kena overlow
+                childAspectRatio: 0.7,
+                // crossAxisSpacing adalah Jarak antara element
+                crossAxisSpacing: 10,
+                // crossAxisCount adalah BERAPA ELEMENT YG INGIN DI GROUP
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, index) {
+                return ItemWidget(products: alldata[index]);
+              },
+            ),
+          );
         },
       ),
     );
